148adf7196de642814cedd9922b789dec781e85c
osh3276 <osh3276@gmail.com>
2025-12-07 01:44:08 -0500

    added vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,22 @@ import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import rehypePrism from 'rehype-prism-plus';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://gooosexe.github.io',
-  
+  site: 'https://gooosexe.github.io',
+
   vite: {
     plugins: [tailwindcss()]
   },
 
   integrations: [mdx()],
-  
+
   markdown: {
     rehypePlugins: [rehypePrism],
     syntaxHighlight: false
-  }
+  },
+
+  adapter: vercel()
 });
