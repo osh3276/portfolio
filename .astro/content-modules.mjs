24259f9c235de6f3306ef4f6319ae603bcad9d43
osh3276 <osh3276@gmail.com>
2026-04-23 20:34:04 -0400

    moving out

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,13 +1,13 @@
 
 export default new Map([
-["src/content/blog/nc-surrounded-regions.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fnc-surrounded-regions.mdx&astroContentModuleFlag=true")],
-["src/content/blog/hi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhi.mdx&astroContentModuleFlag=true")],
-["src/content/blog/new-pen.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fnew-pen.mdx&astroContentModuleFlag=true")],
-["src/content/blog/meeting-rooms-II.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmeeting-rooms-II.mdx&astroContentModuleFlag=true")],
-["src/content/blog/meeting-rooms.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmeeting-rooms.mdx&astroContentModuleFlag=true")],
-["src/content/blog/partition-labels.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fpartition-labels.mdx&astroContentModuleFlag=true")],
 ["src/content/wip/jbcb.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwip%2Fjbcb.mdx&astroContentModuleFlag=true")],
-["src/content/blog/new-stuff.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fnew-stuff.mdx&astroContentModuleFlag=true")],
+["src/content/blog/hi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhi.mdx&astroContentModuleFlag=true")],
+["src/content/blog/meeting-rooms-II.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmeeting-rooms-II.mdx&astroContentModuleFlag=true")],
 ["src/content/unlist/happy-new-year.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Funlist%2Fhappy-new-year.mdx&astroContentModuleFlag=true")],
-["src/content/wip/new-pen.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwip%2Fnew-pen.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/meeting-rooms.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmeeting-rooms.mdx&astroContentModuleFlag=true")],
+["src/content/blog/nc-surrounded-regions.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fnc-surrounded-regions.mdx&astroContentModuleFlag=true")],
+["src/content/blog/moving-out.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmoving-out.mdx&astroContentModuleFlag=true")],
+["src/content/blog/new-stuff.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fnew-stuff.mdx&astroContentModuleFlag=true")],
+["src/content/blog/partition-labels.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fpartition-labels.mdx&astroContentModuleFlag=true")],
+["src/content/blog/new-pen.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fnew-pen.mdx&astroContentModuleFlag=true")]]);
 		
