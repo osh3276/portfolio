4082d56162be519cb6ae7023a842526a96ef0a41
osh3276 <osh3276@gmail.com>
2026-04-29 00:24:56 -0400

    update about me and contact

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,16 +1,16 @@
 
 export default new Map([
-["src/content/blog/game-development.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgame-development.mdx&astroContentModuleFlag=true")],
-["src/content/blog/new-pen.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fnew-pen.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/hi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhi.mdx&astroContentModuleFlag=true")],
-["src/content/blog/new-stuff.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fnew-stuff.mdx&astroContentModuleFlag=true")],
-["src/content/unlist/happy-new-year.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Funlist%2Fhappy-new-year.mdx&astroContentModuleFlag=true")],
-["src/content/unlist/meeting-rooms.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Funlist%2Fmeeting-rooms.mdx&astroContentModuleFlag=true")],
-["src/content/unlist/partition-labels.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Funlist%2Fpartition-labels.mdx&astroContentModuleFlag=true")],
+["src/content/blog/new-pen.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fnew-pen.mdx&astroContentModuleFlag=true")],
 ["src/content/wip/loser.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwip%2Floser.mdx&astroContentModuleFlag=true")],
-["src/content/blog/moving-out.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmoving-out.mdx&astroContentModuleFlag=true")],
+["src/content/blog/game-development.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgame-development.mdx&astroContentModuleFlag=true")],
 ["src/content/wip/makingmaps.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwip%2Fmakingmaps.mdx&astroContentModuleFlag=true")],
-["src/content/unlist/nc-surrounded-regions.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Funlist%2Fnc-surrounded-regions.mdx&astroContentModuleFlag=true")],
+["src/content/blog/new-stuff.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fnew-stuff.mdx&astroContentModuleFlag=true")],
+["src/content/wip/jbcb.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwip%2Fjbcb.mdx&astroContentModuleFlag=true")],
+["src/content/blog/moving-out.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmoving-out.mdx&astroContentModuleFlag=true")],
 ["src/content/unlist/meeting-rooms-II.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Funlist%2Fmeeting-rooms-II.mdx&astroContentModuleFlag=true")],
-["src/content/wip/jbcb.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwip%2Fjbcb.mdx&astroContentModuleFlag=true")]]);
+["src/content/unlist/meeting-rooms.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Funlist%2Fmeeting-rooms.mdx&astroContentModuleFlag=true")],
+["src/content/unlist/nc-surrounded-regions.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Funlist%2Fnc-surrounded-regions.mdx&astroContentModuleFlag=true")],
+["src/content/unlist/partition-labels.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Funlist%2Fpartition-labels.mdx&astroContentModuleFlag=true")],
+["src/content/unlist/happy-new-year.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Funlist%2Fhappy-new-year.mdx&astroContentModuleFlag=true")]]);
 		
