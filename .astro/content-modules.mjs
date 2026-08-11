41566fdc942451e0db4f78207c1bca2ad1097d2f
osh3276 <osh3276@gmail.com>
2026-08-11 13:11:05 -0400

    update gitignore again

--- .astro/content-modules.mjs
+++
@@ -1,9 +0,0 @@
-
-export default new Map([
-["src/content/blog/bike.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbike.mdx&astroContentModuleFlag=true")],
-["src/content/blog/camping.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcamping.mdx&astroContentModuleFlag=true")],
-["src/content/blog/hi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhi.mdx&astroContentModuleFlag=true")],
-["src/content/blog/jbcb.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fjbcb.mdx&astroContentModuleFlag=true")],
-["src/content/blog/moving-out.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmoving-out.mdx&astroContentModuleFlag=true")],
-["src/content/blog/new-pen.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fnew-pen.mdx&astroContentModuleFlag=true")]]);
-		
